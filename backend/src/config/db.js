5d64b782e117760253bf50f4b97cf16430dab71f
akshitapathania0 <akshupathania09@gmail.com>
2026-07-02 12:56:50 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,6 +1,12 @@
 const { PrismaClient } = require("@prisma/client");
 
-const prisma = global.__prisma || new PrismaClient();
+const prisma = global.__prisma || new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 if (process.env.NODE_ENV !== "production") {
   global.__prisma = prisma;
